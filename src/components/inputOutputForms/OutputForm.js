bed21c8747441ab8ba83c7be1098824cfb9791ab
David Muhanguzi <muhunksdavie95@gmail.com>
2019-10-03 16:21:28 +0300

    fix sorting algorithms
    - fix issue preventing display of results from sorting algorithms

--- src/components/inputOutputForms/OutputForm.js
+++ src/components/inputOutputForms/OutputForm.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './InputOutputForms.scss';
 
 const OutputForm = ({ searchResult, timeTaken }) => {
+  // if search result is an empty object, make it an empty string
+  if (Object.entries(searchResult).length === 0 && searchResult.constructor === Object) {
+    searchResult = '';
+  }
   return (
     <div className="col-md-5 workbox-container">
       <h4 className="mt-3">Output</h4>
-      <div className="result-box" dangerouslySetInnerHTML={{ __html: searchResult.resultDataSet }} />
+      <div className="result-box" dangerouslySetInnerHTML={{ __html: searchResult.resultDataSet || searchResult }} />
       <div className="mb-3">
         {searchResult.searchPosition} <br />
         {timeTaken}
